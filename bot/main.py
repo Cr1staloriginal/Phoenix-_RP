c16c27ea04aea09bba072e9f241ee5df3c1908ff
Cr1stalik ˢˢ <dmitrijosipov877@gmail.com>
2026-05-29 20:41:21 +0500

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,14 +1,18 @@
 import asyncio
 import logging
 from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.memory import MemoryStorage
 from bot.config import settings
 from bot.handlers import start_router, character_router, chat_router
+from bot.database import init_db
 
 logging.basicConfig(level=logging.INFO)
 
 async def main():
+    await init_db()  # создаём таблицы в БД
     bot = Bot(token=settings.BOT_TOKEN)
-    dp = Dispatcher()
+    storage = MemoryStorage()
+    dp = Dispatcher(storage=storage)
     dp.include_routers(start_router, character_router, chat_router)
     await dp.start_polling(bot)
 
