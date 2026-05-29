ec0f42b41f06a2f263bb46397157e60a433d740b
Cr1stalik ˢˢ <dmitrijosipov877@gmail.com>
2026-05-29 20:46:25 +0500

    Update __init__.py

---
+++ bot/handlers/__init__.py
@@ -0,0 +1,5 @@
+from .start import router as start_router
+from .character import router as character_router
+from .chat import router as chat_router
+
+__all__ = ["start_router", "character_router", "chat_router"]
